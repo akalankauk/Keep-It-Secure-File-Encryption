b66572155fa3b8e30f278d3660737071eab7d9cc
Akalanka Ekanayake <akalankauk@users.noreply.github.com>
2018-03-11 11:20:30 +0530

    Version 1.5

---
+++ AkalankaUKEncrypt/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace AkalankaUKEncrypt
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.bEncrypt = new System.Windows.Forms.Button();
+            this.bDecrypt = new System.Windows.Forms.Button();
+            this.txStatus = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.filesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ofd
+            // 
+            this.ofd.Multiselect = true;
+            // 
+            // bEncrypt
+            // 
+            this.bEncrypt.Font = new System.Drawing.Font("BatmanForeverAlternate", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bEncrypt.Location = new System.Drawing.Point(12, 192);
+            this.bEncrypt.Name = "bEncrypt";
+            this.bEncrypt.Size = new System.Drawing.Size(326, 34);
+            this.bEncrypt.TabIndex = 1;
+            this.bEncrypt.Text = "🔒 Encrypt My Data";
+            this.bEncrypt.UseVisualStyleBackColor = true;
+            this.bEncrypt.Click += new System.EventHandler(this.bEncrypt_Click);
+            // 
+            // bDecrypt
+            // 
+            this.bDecrypt.Font = new System.Drawing.Font("BatmanForeverAlternate", 15.75F);
+            this.bDecrypt.Location = new System.Drawing.Point(12, 232);
+            this.bDecrypt.Name = "bDecrypt";
+            this.bDecrypt.Size = new System.Drawing.Size(326, 34);
+            this.bDecrypt.TabIndex = 2;
+            this.bDecrypt.Text = "🔓 Decrypt My Data";
+            this.bDecrypt.UseVisualStyleBackColor = true;
+            this.bDecrypt.Click += new System.EventHandler(this.bDecrypt_Click);
+            // 
+            // txStatus
+            // 
+            this.txStatus.Location = new System.Drawing.Point(12, 24);
+            this.txStatus.Multiline = true;
+            this.txStatus.Name = "txStatus";
+            this.txStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txStatus.Size = new System.Drawing.Size(569, 162);
+            this.txStatus.TabIndex = 3;
+            this.txStatus.TextChanged += new System.EventHandler(this.txStatus_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("BatmanForeverAlternate", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 34);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "User Guide";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filesToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.aboutToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(593, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // filesToolStripMenuItem
+            // 
+            this.filesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.filesToolStripMenuItem.Name = "filesToolStripMenuItem";
+            this.filesToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.filesToolStripMenuItem.Text = "Files";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.aboutToolStripMenuItem.Text = "Source Code";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem1.Text = "About";
+            this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.aboutToolStripMenuItem1_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.AccessibleDescription = "";
+            this.pictureBox1.AccessibleName = "";
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(425, 192);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(129, 126);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "";
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(468, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 24);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Info";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AccessibleDescription = "";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(593, 330);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txStatus);
+            this.Controls.Add(this.bDecrypt);
+            this.Controls.Add(this.bEncrypt);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Keep It Secure v1.5";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.OpenFileDialog ofd;
+    private System.Windows.Forms.Button bEncrypt;
+    private System.Windows.Forms.Button bDecrypt;
+    private System.Windows.Forms.TextBox txStatus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem filesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AkalankaUKEncrypt/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AkalankaUKEncrypt
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            ofd.InitialDirectory = "C:";
+        }
+
+        private string GetFileName(string path)
+        {
+            string name = path;
+            int poz = path.LastIndexOf('.');
+            if (poz > 0) name = path.Substring(0, poz);
+            return name;
+        }
+
+        private string GetFileExt(string path)
+        {
+            string ext = "";
+            int poz = path.LastIndexOf('.');
+            if (poz > 0) ext = path.Substring(poz + 1);
+            return ext;
+        }
+
+        private byte ByteEncrypt(byte b)
+        {
+            return (byte)(b ^ 128);
+        }
+
+        private byte[] StrToByteArray(string st, Encoding enc)
+        {
+            return enc.GetBytes(st);
+        }
+
+        private string ByteArrayToStr(byte[] bstr, Encoding enc)
+        {
+            return enc.GetString(bstr);
+        }
+
+        public bool EncryptFile(string inputFile)
+        {
+            string name = GetFileName(inputFile);
+            string ext = GetFileExt(inputFile);
+            byte[] bext = StrToByteArray(ext, new UnicodeEncoding());
+            int k = bext.Length;
+            try
+            {
+                FileStream fsRead = new FileStream(inputFile, FileMode.Open);
+                FileStream fsWrite = new FileStream(name + ".ae", FileMode.Create);
+                fsWrite.Write(BitConverter.GetBytes(k), 0, 4);
+                fsWrite.Write(bext, 0, k);
+                int data;
+                while ((data = fsRead.ReadByte()) != -1) fsWrite.WriteByte(ByteEncrypt((byte)data));
+                fsRead.Close();
+                fsWrite.Close();
+                File.Delete(inputFile);
+                return true;
+            }
+            catch { }
+            return false;
+        }
+
+        public bool DecryptFile(string inputFile)
+        {
+            try
+            {
+                FileStream fsRead = new FileStream(inputFile, FileMode.Open);
+                string name = GetFileName(inputFile);
+                byte[] bint32 = new byte[4];
+                int i = fsRead.Read(bint32, 0, 4);
+                int k = BitConverter.ToInt32(bint32, 0);
+                byte[] bext = new byte[k];
+                i = fsRead.Read(bext, 0, k);
+                string ext = "." + ByteArrayToStr(bext, new UnicodeEncoding());
+                FileStream fsWrite = new FileStream(name + ext, FileMode.Create);
+                int data;
+                while ((data = fsRead.ReadByte()) != -1) fsWrite.WriteByte(ByteEncrypt((byte)data));
+                fsRead.Close();
+                fsWrite.Close();
+                return true;
+            }
+            catch { }
+            return false;
+        }
+
+        private void bEncrypt_Click(object sender, EventArgs e)
+        {
+            txStatus.Text = "";
+            ofd.Filter = "All Files|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string s in ofd.FileNames)
+                {
+                    if (EncryptFile(s)) txStatus.AppendText(s + " has been successfully encrypted" + Environment.NewLine);
+                    else txStatus.AppendText(s + " could not be encrypted" + Environment.NewLine);
+                }
+            }
+        }
+
+        private void bDecrypt_Click(object sender, EventArgs e)
+        {
+            txStatus.Text = "";
+            ofd.Filter = "Encrypted Files|*.ae";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string s in ofd.FileNames)
+                {
+                    if (DecryptFile(s)) txStatus.AppendText(s + " has been successfully decrypted" + Environment.NewLine);
+                    else txStatus.AppendText(s + " could not be decrypted" + Environment.NewLine);
+                }
+            }
+        }
+
+        private void txStatus_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Click '🔒 Encrypt My Data'To Encrypt Your Files");
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/akalankauk/Keep-It-Secure-File-Encryption");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Open Source Project : https://github.com/akalankauk | File Type: .ae",
+    "About");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("When you encrypt your file,original fill will be removed automatically.If you didn't want to remove original file erase 'File.Delete(inputFile);' from Form1.cs (Line Num 64)",
+               "Info");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("When you encrypt your file,original fill will be removed automatically.If you didn't want to remove original file erase 'File.Delete(inputFile);' from Form1.cs (Line Num 64)",
+   "Info");
+        }
+    }
+}
+
